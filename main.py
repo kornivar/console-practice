656f5ca01e8f4f9240eb5247e3c54ba1ea798501
kornivar <kornivar62@gmail.com>
2025-10-30 13:37:25 +0200

    feat : Add the ability to enter numbers

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World") 
+def takeInput():
+    while True:
+        user_input = input("Enter a number: ")
+        try:
+            num = int(user_input)
+            return num 
+        except ValueError:
+            print("You can only enter a number.")
+
+print("Enter two numbers:")
+num1 = takeInput()
+num2 = takeInput()

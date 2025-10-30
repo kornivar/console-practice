ecb01165276172eb616f4ad5ce3c3514e683ee4f
kornivar <kornivar62@gmail.com>
2025-10-30 13:44:24 +0200

    feat : Add the ability to add and subtract numbers

--- main.py
+++ main.py
@@ -7,6 +7,30 @@ def takeInput():
         except ValueError:
             print("You can only enter a number.")
 
-print("Enter two numbers:")
+def takeSign():
+    while True:
+        user_input = input("Enter an operation sign ('+','-'):")
+        if user_input in ("+", "-"):
+            sign = user_input
+            return sign
+        else:
+            print("You can only enter '+' or '-'.")
+
+def addition(a, b):
+    return a + b
+
+def substraction(a, b):
+    return a - b
+
+
+
+print("Enter two numbers")
 num1 = takeInput()
 num2 = takeInput()
+
+sign = takeSign()
+
+if sign == "+":
+    print(f"{num1} + {num2} = {addition(num1, num2)}")
+elif sign == "-":
+    print(f"{num1} - {num2} = {substraction(num1, num2)}")

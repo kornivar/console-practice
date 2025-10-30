66ca17f1f940adfd624a9da340b327c590d17764
kornivar <kornivar62@gmail.com>
2025-10-30 13:51:43 +0200

    feat : Add the ability to raise numbers to a desired power

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def takeInput():
 
 def takeSign():
     while True:
-        user_input = input("Enter an operation sign ('+','-', '*', '/'):")
-        if user_input in ("+", "-", "*", "/"):
+        user_input = input("Enter an operation sign ('+','-', '*', '/', '**'):")
+        if user_input in ("+", "-", "*", "/", "**"):
             sign = user_input
             return sign
         else:
-            print("You can only enter '+', '-', '*' or '/'")
+            print("You can only enter '+', '-', '*', '/' or '**'.")
 
 def addition(a, b):
     return a + b
@@ -31,6 +31,13 @@ def division(a, b):
     else:
         return "\"Just divide it by zero\" - brutal"
 
+def exponentiation(base, power):
+    if power == 0:       
+        return 1
+    if power == 1:
+        return base
+    return base * exponentiation(base, power - 1)
+
 print("Enter two numbers")
 num1 = takeInput()
 num2 = takeInput()
@@ -45,3 +52,5 @@ elif sign == "*":
     print(f"{num1} * {num2} = {multiplication(num1, num2)}")
 elif sign == "/":
     print(f"{num1} / {num2} = {division(num1, num2)}")
+elif sign == "**":
+    print(f"{num1}^{num2} = {exponentiation(num1, num2)}")

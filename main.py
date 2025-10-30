8c1785789a9e9f2b4a10e4f0ba4776f5732aac41
kornivar <kornivar62@gmail.com>
2025-10-30 13:50:14 +0200

    feat : Add the ability to multiply and divide numbers

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ def takeInput():
     while True:
         user_input = input("Enter a number: ")
         try:
-            num = int(user_input)
+            num = float(user_input)
             return num 
         except ValueError:
             print("You can only enter a number.")
 
 def takeSign():
     while True:
-        user_input = input("Enter an operation sign ('+','-'):")
-        if user_input in ("+", "-"):
+        user_input = input("Enter an operation sign ('+','-', '*', '/'):")
+        if user_input in ("+", "-", "*", "/"):
             sign = user_input
             return sign
         else:
-            print("You can only enter '+' or '-'.")
+            print("You can only enter '+', '-', '*' or '/'")
 
 def addition(a, b):
     return a + b
@@ -22,7 +22,14 @@ def addition(a, b):
 def substraction(a, b):
     return a - b
 
+def multiplication(a, b):
+    return a * b
 
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "\"Just divide it by zero\" - brutal"
 
 print("Enter two numbers")
 num1 = takeInput()
@@ -34,3 +41,7 @@ if sign == "+":
     print(f"{num1} + {num2} = {addition(num1, num2)}")
 elif sign == "-":
     print(f"{num1} - {num2} = {substraction(num1, num2)}")
+elif sign == "*":
+    print(f"{num1} * {num2} = {multiplication(num1, num2)}")
+elif sign == "/":
+    print(f"{num1} / {num2} = {division(num1, num2)}")
